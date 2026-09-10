5b0d6c2a6698cd8f182509ad3ba2b65c03d5c1e6
IanHP <129088911+IanHP@users.noreply.github.com>
2026-09-10 14:29:11 -0400

    Feature: add to testing suite

--- promit_search/io/get_type.py
+++ promit_search/io/get_type.py
@@ -1,14 +1,18 @@
 
 from pathlib import Path
 
-def get_type(file: Path) -> str:
+def get_type(file: Path) -> str | None:
     """Returns the type of the file
 
     Args:
         file (Path): file being checked
 
     Returns:
-        str: the type of the file
+        the type of the file, and if it has
+        no suffix it returns None
     """
-
-    return file.suffix[1:]
+    ending: str = file.suffix
+    if ending.startswith("."):
+        return file.suffix[1:]
+    else:
+        return None
---
+++ tests/test_io.py
@@ -0,0 +1,13 @@
+
+from pathlib import Path
+
+from promit_search import io
+
+caffeine_file: Path = Path("input/caffeine.sdf").resolve()
+invalid_file: Path = Path("input/caffeine").resolve()
+
+def get_type_test():
+    if not io.get_type(caffeine_file) == "sdf":
+        raise Exception("Get type invalid for .sdf")
+    if not io.get_type(invalid_file) == None:
+        raise Exception("Get type invalid for invalid file type")
