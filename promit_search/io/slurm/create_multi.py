6ab645582afdf2ad99c2fc1a17b6332536296bc1
IanHP <129088911+IanHP@users.noreply.github.com>
2026-09-10 15:28:51 -0400

    Format: move around input test files

--- promit_search/io/slurm/create_multi.py
+++ promit_search/io/slurm/create_multi.py
@@ -6,13 +6,17 @@ from promit_search.io import text
 
 def create_multi(body: list[str], output_file: Path, settings: dict = {},
                 override_base_settings: bool = False):
-    """_summary_
+    """Creates files required to do parallel batch jobs. Each line
+    in body will be a seperate run. Creates 3 files in directory of output:
+    output_file, job_list.txt, [name of output_file].sh. If they are already
+    present, will override them
 
     Args:
-        body (_type_): _description_
-        output_file (_type_): _description_
-        settings (_type_, optional): _description_. Defaults to {}.
-        override_base_settings (_type_, optional): _description_. Defaults to False.
+        body: all commands to be run. Each will be seperate
+        output_file: where the .slurm file is placed
+            All other files will be put in this directory as well
+        settings: batch settings. Defaults to basic settings.
+        override_base_settings: remove base settings.
     """
     # setup settings
     if not override_base_settings:
--- tests/test_io.py
+++ tests/test_io.py
@@ -8,7 +8,7 @@ from promit_search.io import slurm
 
 def gen_test():
     caffeine_file: Path = Path("input/caffeine.sdf").resolve()
-    invalid_file: Path = Path("input/caffeine").resolve()
+    invalid_file: Path = Path("input/test_io/caffeine").resolve()
     if not gen.get_type(caffeine_file) == "sdf":
         raise Exception("Get type invalid for .sdf")
     if not gen.get_type(invalid_file) == None:
@@ -24,7 +24,7 @@ def gen_test():
 
 
 def csv_test():
-    csv_file: Path = Path("input/test1.csv").resolve()
+    csv_file: Path = Path("input/test_io/test1.csv").resolve()
     test_list: list[list[str]] = [
         ["2","4","6","8"],
         ["10","12","14","16"],
@@ -34,7 +34,7 @@ def csv_test():
     if not test_list == read_list:
         raise Exception("csv read is invalid")
     
-    csv_file: Path = Path("input/test2.csv").resolve()
+    csv_file: Path = Path("input/test_io/test2.csv").resolve()
     test_list: list[list[str]] = [
         ["0","1","2","3","4"],
         ["5","6","7","8","9"],
@@ -49,7 +49,7 @@ def csv_test():
 
 def slurm_test():
     # basic single slurm
-    test_file1: Path = Path("input/check1.slurm").resolve()
+    test_file1: Path = Path("input/test_io/check1.slurm").resolve()
     op_file: Path = Path("output/output.slurm").resolve()
     gen.dir_create(Path("output"))
     body: list[str] = ["line 1","line 2","line 3"]
@@ -63,9 +63,9 @@ def slurm_test():
     op_file.unlink()
 
     # basic multi slurm test
-    test_file1: Path = Path("input/check2.slurm").resolve()
-    test_file2: Path = Path("input/check2.sh").resolve()
-    test_file3: Path = Path("input/job_list.txt").resolve()
+    test_file1: Path = Path("input/test_io/check2.slurm").resolve()
+    test_file2: Path = Path("input/test_io/check2.sh").resolve()
+    test_file3: Path = Path("input/test_io/job_list.txt").resolve()
     op_file: Path = Path("output/output.slurm").resolve()
     op_file2: Path = Path("output/output.sh").resolve()
     op_file3: Path = Path("output/job_list.txt").resolve()
@@ -85,10 +85,10 @@ def slurm_test():
         with open(op_file3, "r") as f2:
             if not f.read() == f2.read():
                 raise Exception("Multi slurm creation invalid - txt")
-
     op_file.unlink()
     op_file2.unlink()
     op_file3.unlink()
+    
     print("slurm_test is valid")
     
 
