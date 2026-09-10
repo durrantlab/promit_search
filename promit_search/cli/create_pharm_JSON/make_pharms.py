9f4ce4d9f6f8dfb1b7704f8eb09f197a0086396c
sodium <129088911+IanHP@users.noreply.github.com>
2026-09-09 22:55:33 -0400

    Feature: start make pharms file

--- promit_search/cli/create_pharm_JSON/make_pharms.py
+++ promit_search/cli/create_pharm_JSON/make_pharms.py
@@ -10,9 +10,16 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 
 
-def make_pharms(file_path: Path, molecules: Chem.Mol, output_dir: Path):
+def make_pharms(file_path: Path, molecules: Chem.Mol, output_dir: Path, as_batch: bool = True,
+                slurm_path: Path | None = None):
+    for mol in molecules:
+        # get name of molecule and create op
+        mol_name: str = mol.GetProp("_Name")
+        output_file = output_dir / f"{mol_name}.json"
+        
+        # run pharmacophore creation
+        # if creating batch job
+        if as_batch:
+            pass
 
-
-    # get name of molecule
-    # create output file name
-    # run pharmacophore creation
+        
--- promit_search/io/sdf/validate.py
+++ promit_search/io/sdf/validate.py
@@ -8,14 +8,26 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
+from .output_rdkit import output_rdkit
+
 
 def validate(sdf_file: Path) -> Chem.SDMolSupplier:
+    """Takes in an SDF file, creates an RDKIT molecule
+    then checks that all are valid
+
+    Args:
+        sdf_file: SDF with molecules
+
+    Raises:
+        Exception: if a molecule in SDF is invalid
+
+    Returns:
+        all molecules in file
+    """
     
     
     # validate SDF and get rdkit molecule(s)
-    supplier = Chem.SDMolSupplier(
-        str(sdf_file), sanitize=True, removeHs=False, strictParsing=True
-    )
+    supplier = output_rdkit(sdf_file)
 
     for mol in supplier:
         if mol == None:
