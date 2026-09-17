e9e780d3ee1c5d6085aff16f7967fa2c0725d40d
IanHP <129088911+IanHP@users.noreply.github.com>
2026-09-17 14:52:02 -0400

    Fix: delete output files before running create_pharm json test

--- tests/test_create_pharm_JSON.py
+++ tests/test_create_pharm_JSON.py
@@ -1,10 +1,13 @@
 
 from promit_search.func.create_pharm_JSON import single_file
 from pathlib import Path
+import shutil
 
 def test_create_pharm_JSON():
     """Status: simply runs, does not validate pharm_json.single_file"""
-    Path("output/create_pharm_json").unlink()
+    if Path("output/create_pharm_json").is_dir():
+        shutil.rmtree(Path("output/create_pharm_json"))
+
     single_file("input/caffeine.sdf","output/create_pharm_json/slurm", create_directories=True, pharmit_run="slurm")
     single_file("input/caffeine.sdf","output/create_pharm_json/web", create_directories=True,  pharmit_run="web")
     single_file("input/caffeine.sdf","output/create_pharm_json/local", create_directories=True,  pharmit_run="local")
