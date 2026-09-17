36c18a3523e2907bac06bab2d7af29a20a2006d4
IanHP <129088911+IanHP@users.noreply.github.com>
2026-09-17 14:18:32 -0400

    Feature: enable logging for test

--- tests/test_pharmit.py
+++ tests/test_pharmit.py
@@ -2,7 +2,9 @@
 from pathlib import Path
 
 from promit_search.pharmit.web import get_pharms
+from promit_search import enable_logging
 
 
 def test_web_search():
+    enable_logging(10)
     get_pharms.main(Path("input/caffeine.sdf"), Path("output/pharmit_search/op.json"))
